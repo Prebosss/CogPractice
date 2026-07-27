409342022b0675db25f426e3d042098c63e40b4a
Prebosss <arias.kenneth@outlook.com>
2026-07-27 14:57:22 -0400

    just base login

--- BankApp.java
+++ BankApp.java
@@ -1,45 +1,88 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class BankApp {
-    public static void main(String[] args) {
-        Bank bank = new Bank(1, "SAVINGS");
-        bank.setBalance(100);
-        System.out.println("User ID: " + bank.getUserId());
-        System.out.println("Account Type: " + bank.getAccountType());
-        System.out.print("Balance: " + bank.getBalance());
+    static Scanner sc = new Scanner(System.in);
+    static Map<String, String> map = new HashMap<>();
+    static {
+        map.put(("admin"), "admin1");
+        map.put("user1", "pass1");
+        map.put("user2", "pass2");
+        map.put("user3", "pass3");
+    }
+
+    public BankApp() {
+    }
+
+    public static void main(String[] args) {    
+        boolean overallLoop = true;
+        while (overallLoop) {
+            String username = login();
+            redirect(username);
+            displayMessage("Do you want to continue? (yes/no)");
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("no")) {
+                overallLoop = false;
+            }
+        }
+    }
+
+    private static String login(){
+        System.out.println("Please enter username and password followed by a space");
+        String input = sc.nextLine();
+        String[] parts = input.split(" ");
+        if (parts.length == 2) {
+            String username = parts[0];
+            String password = parts[1];
+            if (map.containsKey(username) && map.get(username).equals(password)) {
+                return username;
+            }
+        }
+        return null;
+    }
+
+    private static void redirect(String username) {
+        if (username == null || username.isEmpty()) {
+            displayMessage("Invalid Login.");
+        } else if (username.equals("admin")) {
+            adminMenu();
+        } else {
+            userMenu(username);
+        }
+    }
+
+    private static void displayMessage(String message) {
+        System.out.println(message);
+    }
+
+    private static void adminMenu() {
+        displayMessage("Welcome Admin!");
+        // Admin menu logic here
+    }
+
+    private static void userMenu(String username) {
+        displayMessage("Welcome " + username + "!");
+        // User menu logic here
     }
 }
+class Account{
+    private int id;
+    private String name;
 
-class Bank {
-    private int userId;
-    private double balance;
-    private String accountType;
-
-    public Bank(int userId, String accountType) {
-        this.userId = userId;
-        this.balance = 0.0;
-        this.accountType = accountType;
+    public Account(int id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
-    public int getUserId() {
-        return this.userId;
+    public int getId() {
+        return this.id;
     }
 
-    public void setUserId(int userId) {
-        this.userId = userId;
+    public String getName() {
+        return this.name;
     }
 
-    public double getBalance() {
-        return this.balance;
-    }
 
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
 
-    public String getAccountType() {
-        return this.accountType;
-    }
-
-    public void setAccountType(String accountType) {
-        this.accountType = accountType;
-    }
 }
